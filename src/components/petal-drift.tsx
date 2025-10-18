01ac3f1fbcd461e15ded7b4cd7a7e2e9b0864262
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2025-10-18 06:00:09 +0000

    Will this work?
    This is a modified version of petal-drift.tsx
    
    ```tsx
    '

--- src/components/petal-drift.tsx
+++ src/components/petal-drift.tsx
@@ -5,19 +5,34 @@ import { cn } from '@/lib/utils';
 
 function Petal() {
   return (
-    <svg width="14" height="20" viewBox="0 0 14 20" fill="none" xmlns="http://www.w3.org/2000/svg" className="absolute text-primary/70">
-        <path d="M7 20C1.00001 15.1111 -0.4 8.6 1.19999 4.6C2.79999 0.6 7 0 7 0C7 0 11.2 0.6 12.8 4.6C14.4 8.6 13 15.1111 7 20Z" fill="currentColor"/>
-        <path d="M7 20C7.8 17.2 8.5 12.4 7.8 9.2C7.1 6 7 0 7 0" stroke="hsl(var(--primary-foreground) / 0.3)" strokeWidth="0.5"/>
+    <svg
+      width="14"
+      height="20"
+      viewBox="0 0 14 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="absolute text-primary/70"
+    >
+      <path
+        d="M7 20C1.00001 15.1111 -0.4 8.6 1.19999 4.6C2.79999 0.6 7 0 7 0C7 0 11.2 0.6 12.8 4.6C14.4 8.6 13 15.1111 7 20Z"
+        fill="currentColor"
+      />
+      <path
+        d="M7 20C7.8 17.2 8.5 12.4 7.8 9.2C7.1 6 7 0 7 0"
+        stroke="hsl(var(--primary-foreground) / 0.3)"
+        strokeWidth="0.5"
+      />
     </svg>
   );
 }
 
 type PetalStyle = {
   left: string;
+  top: string;
   animationDelay: string;
   animationDuration: string;
+  transform: string;
 };
-
 
 export function PetalDrift({
   className,
@@ -27,26 +42,17 @@ export function PetalDrift({
   quantity?: number;
 }) {
   const [petals, setPetals] = useState<PetalStyle[]>([]);
-  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  useEffect(() => {
-    if (isMounted) {
-      const generatedPetals = Array.from({ length: quantity }, () => ({
-        left: `${Math.random() * 100}vw`,
-        animationDelay: `${Math.random() * 20}s`,
-        animationDuration: `${10 + Math.random() * 10}s`,
-      }));
-      setPetals(generatedPetals);
-    }
-  }, [quantity, isMounted]);
-
-  if (!isMounted) {
-    return null;
-  }
+    const generatedPetals = Array.from({ length: quantity }, () => ({
+      left: `${Math.random() * 100}vw`,
+      top: `${Math.random() * -100}vh`, // random starting Y position above viewport
+      animationDelay: `${Math.random() * 20}s`,
+      animationDuration: `${10 + Math.random() * 10}s`,
+      transform: `rotate(${Math.random() * 360}deg)`,
+    }));
+    setPetals(generatedPetals);
+  }, [quantity]);
 
   return (
     <div
@@ -58,7 +64,7 @@ export function PetalDrift({
       {petals.map((style, i) => (
         <div
           key={i}
-          className="absolute top-0 animate-petal-drift"
+          className="absolute animate-petal-drift"
           style={style as React.CSSProperties}
         >
           <Petal />
